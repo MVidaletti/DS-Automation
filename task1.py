f9357e690cdd84c497373a7884e364cb56e2dcf7
Mateus Vidaletti <mateus.vidaletti@poatek.com>
2021-04-05 16:11:30 -0300

    solving conflicts

--- task1.py
+++ task1.py
@@ -1,12 +1,14 @@
 import csv
 from bot_setup import Bot, BusinessofApps
 
-bot = Bot()
-bot_appdata = BusinessofApps(bot.driver, '/data/app-data/')
-bot_appdata.find_xpath('titles')
-names, years = bot_appdata.split_name_year()
+if __name__ == '__main__':
+    bot = Bot()
+    bot_appdata = BusinessofApps(bot.driver, '/data/app-data/')
+    bot_appdata.find_xpath('titles')
+    names, years = bot_appdata.split_name_year()
 
+    with open('task1.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(zip(names, years))
 
-with open('task1.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerows(zip(names, years))
+    bot.quit()
